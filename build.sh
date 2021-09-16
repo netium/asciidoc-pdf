7aaa0f61bab5926500f59ecf6a9ebc2f8549b837
Bo Zhou <bo.zhou@cerence.com>
2021-09-16 16:34:17 +0800

    Remove asciidoctor-diagram temporarily

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 for f in *.adoc *.ad *.asciidoc *.asc; do
 	echo "Converting asciidoc file: $f"
-	asciidoctor -r asciidoctor-kroki -r asciidoctor-pdf -r asciidoctor-diagram -b pdf -a dot=/usr/bin/dot -a graphvizdot=/usr/bin/dot  -a allow-uri-read $f
-#	asciidoctor -r asciidoctor-kroki -r asciidoctor-pdf -b pdf -a dot=/usr/bin/dot -a graphvizdot=/usr/bin/dot  -a allow-uri-read $f
+#	asciidoctor -r asciidoctor-kroki -r asciidoctor-pdf -r asciidoctor-diagram -b pdf -a dot=/usr/bin/dot -a graphvizdot=/usr/bin/dot  -a allow-uri-read $f
+	asciidoctor -r asciidoctor-kroki -r asciidoctor-pdf -b pdf -a dot=/usr/bin/dot -a graphvizdot=/usr/bin/dot  -a allow-uri-read $f
 done
